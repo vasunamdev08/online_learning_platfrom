c344c48596d516c60ae97f096fcbb30e9c7f5bce
vnamdev <vnamdev@venacorp.com>
2025-05-17 13:06:16 +0530

    initialising project and adding basic project structure

---
+++ src/test/java/com/vena/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vena.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
