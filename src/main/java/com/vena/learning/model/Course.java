c0ae70682baeb0189c0b52cd395e16203bf96aa1
Ankita Porwal <aporwal@venacorp.com>
2025-05-20 11:18:06 +0530

    Imported packages separately

--- src/main/java/com/vena/learning/model/Course.java
+++ src/main/java/com/vena/learning/model/Course.java
@@ -1,7 +1,16 @@
 package com.vena.learning.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Data
