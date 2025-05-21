6ee7e394419df3c88a99777eb97309196aa49b0e
vnamdev <vnamdev@venacorp.com>
2025-05-21 14:43:04 +0530

    adding Admin service and impl.

--- src/main/java/com/vena/learning/service/AdminService.java
+++ src/main/java/com/vena/learning/service/AdminService.java
@@ -4,14 +4,12 @@ import com.vena.learning.dto.RegisterRequest;
 import com.vena.learning.model.Admin;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public interface AdminService {
     void registerAdmin(RegisterRequest adminRequest);
     void saveAdmin(RegisterRequest adminRequest);
     boolean isExists(String email,String username);
-    Optional<Admin> getAdminByEmail(String email);
-    Optional<Admin> getAdminById(String id);
-    Optional<Admin> getAdminByUsername(String username);
+    Admin getAdminByEmail(String email);
+    Admin getAdminById(String id);
+    Admin getAdminByUsername(String username);
 }
