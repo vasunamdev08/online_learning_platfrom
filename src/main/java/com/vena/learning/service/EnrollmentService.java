cf8fd056932001eb65b69edce7fc92a8723447bc
Ankita Porwal <aporwal@venacorp.com>
2025-05-21 16:23:02 +0530

    Added: EnrollmentAPI

---
+++ src/main/java/com/vena/learning/service/EnrollmentService.java
@@ -0,0 +1,18 @@
+package com.vena.learning.service;
+
+import com.vena.learning.dto.EnrollmentRequestDto;
+import com.vena.learning.model.Course;
+import com.vena.learning.model.Enrollment;
+import com.vena.learning.model.Student;
+
+public interface EnrollmentService {
+
+    boolean isEnrolled(String studentId, String courseId);
+    boolean isExists(String studentId, String courseId);
+
+    void unenrollStudent(EnrollmentRequestDto enrollmentRequestDto);
+    void enrollStudent(EnrollmentRequestDto enrollmentRequestDto);
+    void addEnrollment(Student student, Course course);
+
+    Enrollment getCourseDetailsByIds(String studentId, String courseId);
+}
