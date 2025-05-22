0e413b2c05925c64d2ee7c90fa775db2954d7ff3
Ankita Porwal <aporwal@venacorp.com>
2025-05-22 12:18:31 +0530

    Added : getInstitutionByAdminId

--- src/main/java/com/vena/learning/service/AdminService.java
+++ src/main/java/com/vena/learning/service/AdminService.java
@@ -1,19 +1,30 @@
 package com.vena.learning.service;
 
 import com.vena.learning.dto.requestDto.RegisterRequest;
+import com.vena.learning.dto.responseDto.CourseResponse;
+import com.vena.learning.dto.responseDto.UserResponse;
+import com.vena.learning.model.Course;
+import com.vena.learning.model.User;
 import com.vena.learning.model.Admin;
 import org.springframework.stereotype.Service;
+import java.util.List;
+
 
 @Service
 public interface AdminService {
     void registerAdmin(RegisterRequest adminRequest);
     void saveAdmin(RegisterRequest adminRequest);
-
     boolean isExists(String email,String username);
+
     boolean isExistsByEmail(String email);
     boolean isExistsByUsername(String username);
-
     Admin getAdminByEmail(String email);
+
     Admin getAdminById(String id);
     Admin getAdminByUsername(String username);
+
+    List<UserResponse> getAllUsersByInstitution(String adminId);
+    List<CourseResponse> getAllCoursesByInstitution(String adminId);
+
+    String getInstitutionByAdminId(String adminId);
 }
--- src/main/java/com/vena/learning/service/impl/AdminServiceImpl.java
+++ src/main/java/com/vena/learning/service/impl/AdminServiceImpl.java
@@ -1,17 +1,42 @@
 package com.vena.learning.service.impl;
 
+import com.vena.learning.dto.responseDto.CourseResponse;
+import com.vena.learning.dto.responseDto.UserResponse;
+import com.vena.learning.model.Course;
+import com.vena.learning.model.Instructor;
+import com.vena.learning.model.User;
 import com.vena.learning.dto.requestDto.RegisterRequest;
 import com.vena.learning.model.Admin;
 import com.vena.learning.enums.Role;
 import com.vena.learning.repository.AdminRepository;
 import com.vena.learning.service.AdminService;
+import com.vena.learning.service.InstructorService;
+import com.vena.learning.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AdminServiceImpl implements AdminService {
+
     @Autowired
     private AdminRepository adminRepository;
+
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private InstructorService instructorService;
+
+
+    private List<UserResponse> mapToUserResponse(List<User> users) {
+        return users.stream()
+                .map(UserResponse::new)
+                .collect(Collectors.toList());
+    }
 
     @Override
     public void registerAdmin(RegisterRequest adminRequest) {
@@ -71,4 +96,43 @@ public class AdminServiceImpl implements AdminService {
                 () -> new RuntimeException("Admin not found with username: " + username)
         );
     }
+
+    @Override
+    public List<CourseResponse> getAllCoursesByInstitution(String adminId){
+        String institution = getInstitutionByAdminId(adminId);
+        if(institution==null || institution.trim().isEmpty()){
+            throw new IllegalArgumentException("Institution cannot be null or empty");
+        }
+
+        List<Instructor> instructors= instructorService.getAllInstructorByInstitute(institution);
+        List<Course> collect = instructors.stream()
+                .flatMap(instructor -> instructor.getCourses().stream())
+                .collect(Collectors.toList());
+        return collect.stream()
+                .map(CourseResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<UserResponse> getAllUsersByInstitution(String adminId) {
+        String institution = getInstitutionByAdminId(adminId);
+        if(institution==null || institution.trim().isEmpty()){
+            throw new IllegalArgumentException("Institution cannot be null or empty");
+        }
+        List<User> allUsers=new ArrayList<>();
+        allUsers.addAll(studentService.getAllStudentByInstitute(institution));
+        allUsers.addAll(instructorService.getAllInstructorByInstitute(institution));
+        return mapToUserResponse(allUsers);
+    }
+
+    @Override
+    public String getInstitutionByAdminId(String adminId) {
+        Admin admin = adminRepository.findById(adminId).orElseThrow(
+                () -> new RuntimeException("Admin not found with id: " + adminId)
+        );
+        return admin.getInstitution();
+    }
+
+
+
 }
