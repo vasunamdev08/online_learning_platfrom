bb630710d3a6a6c8fb9b5bd6c19898c86ab49025
vnamdev <vnamdev@venacorp.com>
2025-05-22 18:50:07 +0530

    Adding ModuleRequest, ModuleResponse.

---
+++ src/main/java/com/vena/learning/dto/requestDto/ModuleRequest.java
@@ -0,0 +1,14 @@
+package com.vena.learning.dto.requestDto;
+
+import com.vena.learning.enums.Type;
+import lombok.Data;
+
+@Data
+public class ModuleRequest {
+    private String courseId;
+    private int sequence;
+    private String title;
+    private String content;
+    private Type type;
+}
+
