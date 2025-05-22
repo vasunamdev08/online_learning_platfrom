b4cacfd23b31eb430a01297bea6e1cca79403720
Ankita Porwal <aporwal@venacorp.com>
2025-05-22 12:29:53 +0530

    created: CourseResponse Dto

---
+++ src/main/java/com/vena/learning/dto/responseDto/CourseResponse.java
@@ -0,0 +1,18 @@
+package com.vena.learning.dto.responseDto;
+
+import com.vena.learning.model.Course;
+
+public class CourseResponse {
+
+    private String id;
+    private String description;
+    private String title;
+    private String instructorName;
+
+    public CourseResponse(Course course){
+        this.id=course.getId();
+        this.description=course.getDescription();
+        this.title=course.getTitle();
+        this.instructorName= course.getInstructor().getName();
+    }
+}
