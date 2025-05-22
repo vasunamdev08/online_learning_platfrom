f2ef10257450bf8688ec456aa6d17b10cc0e963f
Ankita Porwal <aporwal@venacorp.com>
2025-05-22 12:22:08 +0530

    Added : findByInstitution(string)

--- src/main/java/com/vena/learning/repository/StudentRepository.java
+++ src/main/java/com/vena/learning/repository/StudentRepository.java
@@ -2,13 +2,16 @@ package com.vena.learning.repository;
 
 import com.vena.learning.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.List;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student,String> {
     Optional<Student> getStudentByEmail(String email);
     Optional<Student> findByUsername(String username);
     Optional<Student> findById(String id);
+    Optional<List<Student>> findByInstitution(String institution);
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
