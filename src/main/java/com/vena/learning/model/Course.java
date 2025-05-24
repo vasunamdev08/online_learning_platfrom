82cd0a119c80a682e967f4444e83fe861dc34066
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-05-24 21:22:48 +0530

    Removing: unwanted import in course file.

--- src/main/java/com/vena/learning/model/Course.java
+++ src/main/java/com/vena/learning/model/Course.java
@@ -3,7 +3,6 @@ package com.vena.learning.model;
 import com.vena.learning.dto.requestDto.CourseRequest;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
