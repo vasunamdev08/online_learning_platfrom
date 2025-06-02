59107b769d7334be15bfb00be449f923b108d8af
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-06-02 22:04:49 +0530

    Improved: the code by applying check while fetching module to ensure that the module belongs to that particular course.

--- src/main/java/com/vena/learning/repository/ModuleRepository.java
+++ src/main/java/com/vena/learning/repository/ModuleRepository.java
@@ -2,6 +2,8 @@ package com.vena.learning.repository;
 
 import com.vena.learning.model.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface ModuleRepository extends JpaRepository<Module,String> {
+    Optional<Module> findByIdAndCourseId(String moduleId, String courseId);
 }
