805bdadcb07156997d067f5f9450ffd1087aa33c
AnkitaPorwal25 <aporwal@venacorp.com>
2025-06-04 13:51:29 +0530

    Refactoring

--- src/main/java/com/vena/learning/exception/customException/AdminException/AdminNotFoundByEmailException.java
+++ src/main/java/com/vena/learning/exception/customException/AdminException/AdminNotFoundByEmailException.java
@@ -1,16 +1,15 @@
 package com.vena.learning.exception.customException.AdminException;
 
 public class AdminNotFoundByEmailException extends RuntimeException{
-    public AdminNotFoundByEmailException(String email) {
-        super("Admin not found with email: " + email);
+    public AdminNotFoundByEmailException(String message) {
+        super(message);
     }
-    public AdminNotFoundByEmailException(String email, Throwable cause) {
-        super("Admin not found with email: " + email, cause);
+
+    public AdminNotFoundByEmailException(String message, Throwable cause) {
+        super(message, cause);
     }
-    public AdminNotFoundByEmailException(String email, String message) {
-        super("Admin not found with email: " + email + ". " + message);
-    }
-    public AdminNotFoundByEmailException(String email, String message, Throwable cause) {
-        super("Admin not found with email: " + email + ". " + message, cause);
+
+    public AdminNotFoundByEmailException(Throwable cause) {
+        super(cause);
     }
 }
--- src/main/java/com/vena/learning/exception/customException/AdminException/AdminNotFoundByIdException.java
+++ src/main/java/com/vena/learning/exception/customException/AdminException/AdminNotFoundByIdException.java
@@ -1,19 +1,15 @@
 package com.vena.learning.exception.customException.AdminException;
 
 public class AdminNotFoundByIdException extends RuntimeException{
-    public AdminNotFoundByIdException(String id) {
-        super("Admin not found with id: " + id);
+    public AdminNotFoundByIdException(String message) {
+        super(message);
     }
 
-    public AdminNotFoundByIdException(String id, Throwable cause) {
-        super("Admin not found with id: " + id, cause);
+    public AdminNotFoundByIdException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    public AdminNotFoundByIdException(String id, String message) {
-        super("Admin not found with id: " + id + ". " + message);
-    }
-
-    public AdminNotFoundByIdException(String id, String message, Throwable cause) {
-        super("Admin not found with id: " + id + ". " + message, cause);
+    public AdminNotFoundByIdException(Throwable cause) {
+        super(cause);
     }
 }
