98bb21d88222a1795a2dd1d9d8301ceb13a47097
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-06-04 09:30:59 +0530

    Implemented: the exception handler for the quiz, student and user exception.

--- src/main/java/com/vena/learning/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vena/learning/exception/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.vena.learning.exception;
 
 import com.vena.learning.exception.customException.AdminException.AdminNotFoundByEmailException;
 import com.vena.learning.exception.customException.AdminException.AdminNotFoundByIdException;
+import com.vena.learning.exception.customException.QuizException.*;
+import com.vena.learning.exception.customException.StudentException.*;
+import com.vena.learning.exception.customException.UserException.UserDeletionNotAuthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
@@ -54,5 +57,90 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_PROBLEM_JSON).body(problemDetail);
     }
 
-    //----------------Student Exception Handler
+    // ------------ Quiz Exceptions --------------
+
+    @ExceptionHandler(ChoiceNotFoundException.class)
+    public ResponseEntity<String> handleChoiceNotFound(ChoiceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MaxQuizAttemptsExceededException.class)
+    public ResponseEntity<String> handleMaxQuizAttempts(MaxQuizAttemptsExceededException e) {
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(e.getMessage());
+    }
+
+    @ExceptionHandler(QuizNotFoundException.class)
+    public ResponseEntity<String> handleQuizNotFound(QuizNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    // ------------ Student Exceptions -------------
+
+    @ExceptionHandler(EnrollmentDoesNotExistException.class)
+    public ResponseEntity<String> handleEnrollmentDoesNotExist(EnrollmentDoesNotExistException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(EnrollmentNotFoundException.class)
+    public ResponseEntity<String> handleEnrollmentNotFound(EnrollmentNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(NoCoursesFoundForStudentException.class)
+    public ResponseEntity<String> handleNoCoursesFound(NoCoursesFoundForStudentException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentAlreadyEnrolledException.class)
+    public ResponseEntity<String> handleStudentAlreadyEnrolled(StudentAlreadyEnrolledException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentAlreadyExistsException.class)
+    public ResponseEntity<String> handleStudentAlreadyExists(StudentAlreadyExistsException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentDetailIncompleteException.class)
+    public ResponseEntity<String> handleStudentDetailIncomplete(StudentDetailIncompleteException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentNotEnrolledInCourseException.class)
+    public ResponseEntity<String> handleStudentNotEnrolled(StudentNotEnrolledInCourseException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentNotFoundByEmailException.class)
+    public ResponseEntity<String> handleStudentNotFoundByEmail(StudentNotFoundByEmailException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentNotFoundByIdException.class)
+    public ResponseEntity<String> handleStudentNotFoundById(StudentNotFoundByIdException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentNotFoundByUsername.class)
+    public ResponseEntity<String> handleStudentNotFoundByUsername(StudentNotFoundByUsername e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<String> handleStudentNotFound(StudentNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(StudentViewNotAuthorizedException.class)
+    public ResponseEntity<String> handleStudentViewNotAuthorized(StudentViewNotAuthorizedException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+
+    // ------------- User Exceptions -----------------
+
+    @ExceptionHandler(UserDeletionNotAuthorizedException.class)
+    public ResponseEntity<String> handleUserDeletionNotAuthorized(UserDeletionNotAuthorizedException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+
 }
